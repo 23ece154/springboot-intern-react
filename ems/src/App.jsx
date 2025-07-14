b495f4d8a510c2df80caef9f5b80495c6bdb37c2
sri karthika <srikarthika.l2023ece@sece.ac.in>
2025-07-14 13:57:09 +0530

    1st commit

---
+++ ems/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Login from './components/Login'; // adjust the path based on your project structure
+
+
+function App() {
+  return(
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+
+export default App
