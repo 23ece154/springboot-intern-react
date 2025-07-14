93c29d78d454bbec76348fe5f7e937b5568d3cdf
sri karthika <srikarthika.l2023ece@sece.ac.in>
2025-07-14 14:56:39 +0530

    ✨ Added Register page and updated App routing

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,15 +1,17 @@
-
-import './App.css'
-import Login from './components/Login'; // adjust the path based on your project structure
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  return(
-    <div>
-      <Login />
-    </div>
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} /> {/* 👈 default to register */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
